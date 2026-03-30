d943c202caacc54aee1031f219be7a5fc691e83b
yfree1232-afk <yfree1232@gmail.com>
2026-03-30 18:50:03 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,29 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+from os import environ 
+
+class Config:
+    API_ID = int(environ.get("API_ID", ""))
+    API_HASH = environ.get("API_HASH", "")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
+    DATABASE_URI = environ.get("DATABASE_URI", "")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
+    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+class temp(object): 
+    lock = {}
+    CANCEL = {}
+    forwardings = 0
+    BANNED_USERS = []
+    IS_FRWD_CHAT = []
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
